125be35235f900eb5096aea9f55c449ff61fdbae
james1998stuart <james1998stuart@gmail.com>
2025-05-05 08:25:57 -0400

    Update CraftingCalculator.bat
    changed python back to py

--- ScriptingProject/CraftingCalculator.bat
+++ ScriptingProject/CraftingCalculator.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 echo Launching Crafting Calc... 
-python ScriptingProject\ScriptingProject\ScriptingProject.py
+py ScriptingProject\ScriptingProject\ScriptingProject.py
 pause
 
